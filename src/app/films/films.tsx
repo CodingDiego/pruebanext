4508f1a901fa62bcf0d357aea04e2ee6a20a7fc7
Diego Palacios <palaciosdiegovalentino@gmail.com>
2024-05-10 15:03:30 -0300

    ADD: Films page initial file commit & api file

---
+++ src/app/films/films.tsx
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Films({ }) {
+
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            <>Films page</>
+        </div>
+    );
+}
+
