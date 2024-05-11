e09db5d0e20ebffb3095bdadc66e69b78e6a5916
Diego Palacios <palaciosdiegovalentino@gmail.com>
2024-05-10 22:58:43 -0300

    ADD: Dynamic Id Character File for later developing

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,8 +1,28 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { fetchCharacters } from '../api';
+import { Character } from '../lib/types/character';
 
-export default function Characters() {
+export default async function Characters() {
+    const data = await fetchCharacters();
+
     return (
-        <>
-            <h1>Personajes</h1>
-        </>
-    )
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 items-center justify-center min-h-screen py-2">
+            {data.map((character: Character) => (
+                <div key={character.name} className="m-4 transform transition-transform duration-500 hover:scale-110">
+                    <Link href={`/characters/${character.name}`}>
+                        <button>
+                            <Image src='/genericSW.jpg'
+                                alt='Character Picture'
+                                width={200}
+                                height={300} />
+                            <div className="h-12">
+                                <h1 className="text-xl">{character.name}</h1>
+                            </div>
+                        </button>
+                    </Link>
+                </div>
+            ))}
+        </div>
+    );
 }
