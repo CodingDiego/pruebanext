536094f89257b41978d513660167d13c6a967091
Diego Palacios <palaciosdiegovalentino@gmail.com>
2024-05-11 04:11:11 -0300

    Fix: Films' page styling for overflow problems

--- src/app/films/page.tsx
+++ src/app/films/page.tsx
@@ -7,10 +7,10 @@ export default async function Films() {
     const data = await fetchFilms();
 
     return (
-        <div className="flex flex-row overflow-x-scroll items-center justify-center min-h-screen py-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 items-center justify-center min-h-screen py-2 overflow-hidden">
             {data.map((film: Film) => (
-                <div key={film.episode_id} className="m-4 transform transition-transform duration-500 hover:scale-110">
-                    <h4 className="text-center bg-gray-600 p-2">Episode {film.episode_id}</h4>
+                <div key={film.episode_id} className="m-4 transform transition-transform duration-500 hover:scale-110 text-center">
+                    <h4 className="text-center bg-gray-600 p-2 w-full sm:w-auto mx-auto max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg">Episode {film.episode_id}</h4>
                     <Link href={`/films/${film.episode_id}`}>
                         <button>
                             <Image src='/genericSW.jpg'
