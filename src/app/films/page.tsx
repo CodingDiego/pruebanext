c484e8d971929fed477e52cd4548e78398fa8227
Diego Palacios <palaciosdiegovalentino@gmail.com>
2024-05-10 22:57:16 -0300

    FIX: Code for Films Fetch handling and Styling for better display

--- src/app/films/page.tsx
+++ src/app/films/page.tsx
@@ -1,13 +1,29 @@
-import axios from 'axios';
 import Link from 'next/link';
 import Image from 'next/image';
+import { fetchFilms } from '../api';
+import { Film } from '../lib/types/film';
 
-export default function Films({ }) {
+export default async function Films() {
+    const data = await fetchFilms();
 
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <>Films page</>
+        <div className="flex flex-row overflow-x-scroll items-center justify-center min-h-screen py-2">
+            {data.map((film: Film) => (
+                <div key={film.episode_id} className="m-4 transform transition-transform duration-500 hover:scale-110">
+                    <h4 className="text-center bg-gray-600 p-2">Episode {film.episode_id}</h4>
+                    <Link href={`/films/${film.episode_id}`}>
+                        <button>
+                            <Image src='/genericSW.jpg'
+                                alt='Movie Picture'
+                                width={250}
+                                height={200} />
+                            <div className="h-12">
+                                <h1 className="text-xl">{film.title}</h1>
+                            </div>
+                        </button>
+                    </Link>
+                </div>
+            ))}
         </div>
     );
 }
-
