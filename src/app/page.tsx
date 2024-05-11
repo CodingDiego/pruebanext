9e2fe160ee14ee45d41d730c0fb2b93195b3fa19
Diego Palacios <palaciosdiegovalentino@gmail.com>
2024-05-11 04:10:16 -0300

    Refactor: Landing Page Styling & Design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,17 @@
-import Image from "next/image";
 import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-star-wars bg-main">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center">
-      </div>
-
-      <div className="flex justify-between items-center w-full lg:w-1/2">
+    <main className="flex flex-col items-center justify-between px-4 py-8 bg-black text-white min-h-max">
+      <h1 className="text-4xl font-bold mb-8">Star Wars Landing Page</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 items-center w-full lg:w-1/2">
         <Link href="/characters">
-          <button className="btn-blue">
+          <button className="btn-blue bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded w-full">
             Characters
           </button>
         </Link>
-
         <Link href="/films">
-          <button className="btn-blue">
+          <button className="btn-blue bg-yellow-500 hover:bg-yellow-700 text-black font-bold py-2 px-4 rounded w-full">
             Films
           </button>
         </Link>
