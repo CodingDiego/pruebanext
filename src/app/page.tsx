25ecafa4f9876b0ca66262ebc31c92b1ab824541
Diego Palacios <palaciosdiegovalentino@gmail.com>
2024-05-10 22:58:15 -0300

    Delete: Unnecesary Comment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-star-wars bg-main">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        {/* No additional text here */}
       </div>
 
       <div className="relative z-[-1] flex place-items-center">
-        {/* No Star Wars logo here */}
       </div>
 
       <div className="flex justify-between items-center w-full lg:w-1/2">
